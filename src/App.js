47441d1e853f944d2ff0e0361d8f1d0429c8c720
marcosjanuario <januario.marcos@icloud.com>
2022-05-26 20:49:42 +0200

    adding item to cart

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from "./components/Cart/Cart";
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -16,7 +17,7 @@ function App() {
   };
 
   return (
-    <React.Fragment>
+    <CartProvider>
       {
         cartIsShown && <Cart onCloseCart={hideCartHandler} />
       }
@@ -24,7 +25,7 @@ function App() {
       <main>
         <Meals />
       </main>
-    </React.Fragment>
+    </CartProvider>
   );
 }
 
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -31,13 +31,14 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = (props) => {
   const mealsList = DUMMY_MEALS
-    .map(meal => <MealItem
-      id={meal.id}
-      key={meal.id}
-      name={meal.name}
-      description={meal.description}
-      price={meal.price}/>
-    );
+    .map(meal =>
+      <MealItem
+        id={meal.id}
+        key={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}/>
+      );
 
   return (
     <section className={classes.meals}>
